4ee3810f7b0c8bf5954436e4876763f5bdc1cde9
xyzFahim <xyzfahim99@gmail.com>
2024-08-20 04:50:26 +0600

    MongoDB Connected

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const connectDB = require('./Config/db');
 
 //Parse request
 app.use(bodyParser.json());
+
+//MongoDB connection
+connectDB()
 
 const port = 5000
 app.listen(port , ()=> {
